e014bb56fd74aafa56325573b9b35842e45c4fa7
Metwet <tselm.matv@gmail.com>
2023-06-25 17:23:26 +0300

    modified service Meetup

--- src/app/services/meetup.service.ts
+++ src/app/services/meetup.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Meetup } from '../models/meetup.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MeetupService {
 
+  private meetups: Meetup[] = [];
+
   constructor() { }
+
+  getMeetups(): Meetup[] {
+    return this.meetups;
+  }
+
+  addMeetup(meetup: Meetup):void {
+    this.meetups.push(meetup);
+  }
+
+  updateMeetup(meetup: Meetup):void {
+
+  }
+
+  deleteMeetup(meetup: Meetup): void {
+    const index = this.meetups.indexOf(meetup);
+    if(index > -1){
+      this.meetups.splice(index, 1);
+    }
+  }
 }
