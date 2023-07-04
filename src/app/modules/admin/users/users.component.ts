f723b52781e0d234c0d0594fcae01d7f51eb80e9
Metwet <tselm.matv@gmail.com>
2023-07-04 07:49:45 +0300

    add change and delete user

--- src/app/modules/admin/users/users.component.ts
+++ src/app/modules/admin/users/users.component.ts
@@ -20,11 +20,48 @@ export class UsersComponent {
   loadUsers():void {
     this.userService.getUsers().subscribe(
       (users:User[])=>{
-
         console.log(users);
         
         this.users = users;
       }
     )
+  }
+
+  updateUser(user: User): void {
+    const updatedUser = {
+      email: user.email,
+      password: user.password,
+      fio: user.fio
+    };
+    console.log(updatedUser);
+    this.userService.updateUser(user.id, updatedUser).subscribe(
+      (res: any) => {
+        console.log('Пользователь успешно обновлен:', res);
+      }
+    );
+  }
+
+  updateRole(user: User): void {
+    const selectedRole = user.roles[0].name === 'ADMIN' ? ['ADMIN'] : ['USER'];
+    const roleData = {
+      names: selectedRole,
+      userId: user.id
+    };
+    console.log(roleData);
+    
+    this.userService.updateUserRole(roleData).subscribe(
+      (res: any) => {
+        console.log('Роль пользователя обновлена:', res); 
+      }
+    );
+  }
+
+  deleteUser(userId: number): void {
+    this.userService.deleteUser(userId).subscribe(
+      (res) => {
+        console.log('Пользователь удален:', res)
+        this.loadUsers();
+      }
+    );
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,11 +9,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UserService {
 
-  private baseUrl: string = `${environment.backendOrigin}`;
+  private baseUrl: string = `${environment.backendOrigin}/user`;
  
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(`${this.baseUrl}/user`);
+    return this.http.get<User[]>(`${this.baseUrl}`);
+  }
+
+  updateUser(userId: number, userData: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${userId}`, userData);
+  }
+
+  updateUserRole(roleData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/role`, roleData);
+  }
+
+  deleteUser(userId: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${userId}`);
   }
 }
